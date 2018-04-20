09fe5a99d49393f52e786b0e8d91b9d7d8e7c8b9
Alex Sheu <alex.sheu@duke.edu>
2018-04-19 22:20:29 -0400

    added equalization and contrast stretching code. uploaded image encode code

--- Filtering.py
+++ Filtering.py
@@ -1,22 +1,38 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+from skimage import data, img_as_float
+from skimage import exposure
 
+def Histogram_Eq(img):
+    equal = exposure.equalize_hist(img)
+    return equal
 
+def Contrast_Stretching(img):
+    p2, p98 = np.percentile(img, (2, 98))
+    contrast= exposure.rescale_intensity(img, in_range=(p2, p98))
+    return contrast
 
-def Histogram_Eq():
-	pass
+def raw_hist(img):
+    plt.hist(img.ravel(), 256, [0, 256]);
+    plt.show()
 
+def equalization_hist(img):
+    plt.hist(img.ravel(), 256, [0, 1]);
+    plt.show()
 
+def contrast_hist(img):
+    plt.hist(img.ravel(), 256, [0, 256]);
+    plt.show()
 
-def Contrast_Streching():
-	pass
+def show_img(img):
+    plt.imshow(img)
+    plt.show()
 
-
-def Log_Compression():
-	pass
-
-
-
-def Reverse_Video():
-	pass
-
+def importImage(fileName):
+    image = cv2.imread(fileName)
+    return image
 
 

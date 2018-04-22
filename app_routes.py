2401b79add49fe3815c3ad791e08c59ea6a85811
Alex Sheu <alex.sheu@duke.edu>
2018-04-22 17:55:05 -0400

    fixed pep8 formatting

--- app_routes.py
+++ app_routes.py
@@ -6,57 +6,46 @@ import datetime, numpy
 app = Flask(__name__)
 connect("mongodb://vcm-3579.vm.duke.edu:27017/heart_rate_app")
 
-@app.route('/<email>/user_action', methods = ['GET'])
-def userStats(email):
 
-	''' Function is a GET request that allows user to access the statistics of filter functions
-	queried based on the email address input '''
+@app.route('/<email>/user_action', methods=['GET'])
+def user_stats(email):
+    """Function is a GET request that allows user to access the statistics of filter functions 
+queried based on the email address input """
+    mail = "{0}".format(email)
+    try:
+        user = models.User.objects.raw({"_id": mail}).first()
+        data = user.user_action
+        return jsonify(data), 200
+    
+    except:
+        data = 'User does not exist'
+        return jsonify(data), 404
 
-	mail = "{0}".format(email)
 
-	try:
-
-		user = models.User.objects.raw({"_id": mail}).first()
-		data = user.user_action
-		return jsonify(data), 200
-
-	except:
-
-		data = 'User does not exist'
-		return jsonify(data), 404
-
-@app.route('/<email>/images', methods = ['GET'])
+@app.route('/<email>/images', methods=['GET'])
 def images(email):
+    
+    """Function is a GET request that allows user to access all image file names that 
+    have been uploaded"""
+    mail = "{0}".format(email)
+    try:
+        user = models.User.objects.raw({"_id": mail}).first()
+        data = {'Uploaded Images': str(user.image_names)}
+        return jsonify(data), 200
+    except:
+        data = 'User does not exist'
+        return jsonify(data), 404
 
-	''' Function is a GET request that allows user to access all image file names that have been uploaded'''
 
-	mail = "{0}".format(email)
-
-	try:
-
-		user = models.User.objects.raw({"_id": mail}).first()
-		data = {'Uploaded Images': str(user.image_names)}
-		return jsonify(data), 200
-
-	except:
-
-		data = 'User does not exist'
-		return jsonify(data), 404
-
-@app.route('/<email>/latency', methods = ['GET'])
+@app.route('/<email>/latency', methods=['GET'])
 def latency(email):
+    """ Function is a GET request that allows user to access the latency data'"""	
+    mail = "{0}".format(email)
+    try:
+        user = models.User.objects.raw({"_id": mail}).first()
+        data = {'Latency for Processes': str(user.latency)}
+        return jsonify(data), 200
+    except:
+        data = 'User does not exist'
+        return jsonify(data), 404
 
-	''' Function is a GET request that allows user to access the latency data'''
-
-	mail = "{0}".format(email)
-
-	try:
-
-		user = models.User.objects.raw({"_id": mail}).first()
-		data = {'Latency for Processes': str(user.latency)}
-		return jsonify(data), 200
-
-	except:
-
-		data = 'User does not exist'
-		return jsonify(data), 404

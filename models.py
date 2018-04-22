b18c12749119ecb5474b0b79aed5630ed6113902
ttw6 <35498647+ttw6@users.noreply.github.com>
2018-04-22 18:21:08 -0400

    Delete whitespace

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class User(MongoModel):
     image_names = fields.ListField(field=fields.CharField())
     # stores the names (file names) for different images
     user_action = fields.DictField()
-    # stores the amount of times the different user actions have 
+    # stores the amount of times the different user actions have
     # been run (all filtering options)
     latency = fields.ListField(field=fields.IntegerField())
     # stores the latency of each of the user actions

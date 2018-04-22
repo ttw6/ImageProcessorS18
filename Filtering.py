41f68b97690d7b8588955996a28dc37ad5cc4696
Petek Sener <32279618+ps178@users.noreply.github.com>
2018-04-22 18:00:54 -0400

    Update Filtering.py

--- Filtering.py
+++ Filtering.py
@@ -11,12 +11,12 @@ def Image_String(filename):
         image_string = base64.b64encode(image_file.read())
         return image_string
 
-    
+ 
 def Save_Image_String(base64image, filename):
     with open(filename, 'wb') as Image_Out:
         Image_Out.write(base64.b64decode(base64image))
 
-        
+    
 def Histogram_Eq():
     pass
 
@@ -33,5 +33,3 @@ def Reverse_Video():
     im = skimage.io.imread("/Users/PetekSener/Documents/AT2.jpg")
     im_inverted = skimage.util.invert(im)
     skimage.io.imsave("hope.jpg", im_inverted)
-    
-    

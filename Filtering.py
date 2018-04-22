43eb876a8da6a950c054557cf2fd27f9a22c371d
Tiffany Wu <tiffwu88@gmail.com>
2018-04-22 16:54:36 -0400

    Reformat code and added docstrings

--- Filtering.py
+++ Filtering.py
@@ -2,9 +2,16 @@ from skimage import exposure, io
 
 
 def main():
-    image1 = io.imread('sample2.jpg', as_grey=True) # look into user warning
-    out1 = Log_Compression(image1)
-    io.imsave('log_comp2.jpg', out1)
+    name = 'sample1'
+    Log_Compression(name)
+
+    '''
+    Uncomment block for multiple files
+    
+    name = ['sample1', 'sample2']
+    for n, val in enumerate(name):
+        Log_Compression(name[n])
+    '''
 
 
 def Histogram_Eq():
@@ -15,9 +22,20 @@ def Contrast_Streching():
     pass
 
 
-def Log_Compression(image):
+def Log_Compression(file_name, file_type='.jpg'):
+    """
+    Function will replace pixel value with its logarithm (effectively enhancing
+    low intensity pixel values).
+
+    :param file_name: Name of file
+    :param file_type: File type (default is .jpg)
+    :return: output image (in .jpg)
+    """
+    image = io.imread(file_name + file_type,
+                      as_grey=True)  # look into user warnings
     log_out = exposure.adjust_log(image)
-    return log_out
+    output = io.imsave(file_name + '_log.jpg', log_out)
+    return output
 
 
 def Reverse_Video():

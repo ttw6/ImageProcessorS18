d841214ab6a266f789c95a68d4ef2116c5c7960f
Alex Sheu <alex.sheu@duke.edu>
2018-04-22 18:15:13 -0400

    pep8

--- app_routes.py
+++ app_routes.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from pymodm import connect, MongoModel, fields
 from models import *
-import datetime, numpy
+import datetime
+import numpy
 
 app = Flask(__name__)
 connect("mongodb://vcm-3579.vm.duke.edu:27017/heart_rate_app")
@@ -9,9 +10,9 @@ connect("mongodb://vcm-3579.vm.duke.edu:27017/heart_rate_app")
 
 @app.route('/<email>/user_action', methods=['GET'])
 def user_stats(email):
-    """Function is a GET request that allows user to  
-    access the statistics of filter functions  queried based 
-    on the email address input """
+    """Function is a GET request that allows user to
+    access the statistics of filter functions  queried based
+    on the email address input"""
     mail = "{0}".format(email)
     try:
         user = models.User.objects.raw({"_id": mail}).first()
@@ -25,7 +26,7 @@ def user_stats(email):
 
 @app.route('/<email>/images', methods=['GET'])
 def images(email):
-    """Function is a GET request that allows user to 
+    """Function is a GET request that allows user to
     access all image file names that have been uploaded"""
     mail = "{0}".format(email)
     try:
@@ -39,8 +40,8 @@ def images(email):
 
 @app.route('/<email>/latency', methods=['GET'])
 def latency(email):
-    """ Function is a GET request that allows 
-    user to access the latency data'"""
+    """Function is a GET request that allows
+    user to access the latency data"""
     mail = "{0}".format(email)
     try:
         user = models.User.objects.raw({"_id": mail}).first()
@@ -49,3 +50,4 @@ def latency(email):
     except:
         data = 'User does not exist'
         return jsonify(data), 404
+

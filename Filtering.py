5e3f88ba3d20cab2e43638e8bf87e61759446a33
Petek Sener <peteksener9@gmail.com>
2018-04-22 17:41:40 -0400

    Filtering and front end design

--- Filtering.py
+++ Filtering.py
@@ -1,5 +1,19 @@
+import skimage
+import base64
 
 
+from skimage import io
+from skimage import util
+
+
+def Image_String(filename):
+    with open(filename,'rb') as image_file:
+	    image_string = base64.b64encode(image_file.read())
+	    return image_string
+
+def Save_Image_String(base64image, filename):
+    with open(filename, "wb") as Image_Out:
+	    Image_Out.write(base64.b64decode(base64image))
 
 def Histogram_Eq():
 	pass
@@ -16,7 +30,10 @@ def Log_Compression():
 
 
 def Reverse_Video():
-	pass
+    im = skimage.io.imread("/Users/PetekSener/Documents/AT2.jpg")
+    im_inverted = skimage.util.invert(im)
+    skimage.io.imsave("hope.jpg",im_inverted)
+    
 
 
 

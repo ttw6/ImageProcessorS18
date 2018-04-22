68c4f851fad78d3166ec3aacc0f7f67cf085ed08
Tiffany Wu <tiffwu88@gmail.com>
2018-04-21 21:50:32 -0400

    Add log comp fn

--- Filtering.py
+++ Filtering.py
@@ -1,22 +1,28 @@
+from skimage import exposure, io
 
+
+def main():
+    image1 = io.imread('sample2.jpg', as_grey=True) # look into user warning
+    out1 = Log_Compression(image1)
+    io.imsave('log_comp2.jpg', out1)
 
 
 def Histogram_Eq():
-	pass
-
+    pass
 
 
 def Contrast_Streching():
-	pass
+    pass
 
 
-def Log_Compression():
-	pass
-
+def Log_Compression(image):
+    log_out = exposure.adjust_log(image)
+    return log_out
 
 
 def Reverse_Video():
-	pass
+    pass
 
 
-
+if __name__ == "__main__":
+    main()

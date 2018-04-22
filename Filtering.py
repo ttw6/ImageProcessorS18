0fd8c36c9e1b0cc461972abe729b4e9c424c429b
Alex Sheu <alex.sheu@duke.edu>
2018-04-22 17:42:42 -0400

    fixed pep8

--- Filtering.py
+++ Filtering.py
@@ -6,33 +6,38 @@ import cv2
 from skimage import data, img_as_float
 from skimage import exposure
 
-def Histogram_Eq(img):
+
+def hist(img):
+    # image = img_as_float(img)
     equal = exposure.equalize_hist(img)
     return equal
 
-def Contrast_Stretching(img):
+
+def contrast_stretching(img):
     p2, p98 = np.percentile(img, (2, 98))
     contrast= exposure.rescale_intensity(img, in_range=(p2, p98))
     return contrast
 
+
 def raw_hist(img):
-    plt.hist(img.ravel(), 256, [0, 256]);
+    plt.hist(img.ravel(), 256, [0, 256])
     plt.show()
+
 
 def equalization_hist(img):
-    plt.hist(img.ravel(), 256, [0, 1]);
+    plt.hist(img.ravel(), 256, [0, 1])
     plt.show()
 
+
 def contrast_hist(img):
-    plt.hist(img.ravel(), 256, [0, 256]);
+    plt.hist(img.ravel(), 256, [0, 256])
     plt.show()
+
 
 def show_img(img):
     plt.imshow(img)
     plt.show()
 
-def importImage(fileName):
-    image = cv2.imread(fileName)
-    return image
+
 
 

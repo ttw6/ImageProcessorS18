f5003899a13de45b277da2adb355d1782640c512
ttw6 <35498647+ttw6@users.noreply.github.com>
2018-04-29 22:05:01 -0400

    Comment out extraneous test

--- test_log_compression.py
+++ test_log_compression.py
@@ -1,13 +1,13 @@
 import pytest
 
 
-def test_strip_ext():
-    from Filtering import strip_ext
+# def test_strip_ext():
+#    from Filtering import strip_ext
     # Check functionality
-    test_name = 'sample1.jpg'
-    file_name, file_ext = strip_ext(test_name)
-    assert file_name == 'sample1'
-    assert file_ext == '.jpg'
+#    test_name = 'sample1.jpg'
+#    file_name, file_ext = strip_ext(test_name)
+#    assert file_name == 'sample1'
+#    assert file_ext == '.jpg'
 
 
 def test_log_comp():

1888f4305f5ba2d48dc28837ee41b6dd5557711b
ttw6 <35498647+ttw6@users.noreply.github.com>
2018-04-29 22:19:19 -0400

    Add strip_header fn

--- Filtering.py
+++ Filtering.py
@@ -112,3 +112,16 @@ def rev_hist(file_name, img):
     """
     plt.hist(img.ravel(), 256, [0, 256])
     plt.savefig(file_name + '4_hist.jpg')
+
+
+def strip_header(base64_str):
+    """
+    Strips the data url header
+    :param base64_str: Raw base64 string
+    :return: Stripped string
+    """
+    try:
+        stripped = base64_str.split(',')[1]
+    except AttributeError:
+        return "Check if base64 is string"
+    return stripped

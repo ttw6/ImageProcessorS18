600c6b9f7b7bac3a5370518fed64719a620c8ec5
Tiffany Wu <tiffwu88@gmail.com>
2018-05-01 21:00:57 -0400

    Updated all tests for Filtering.py fn

--- tests/test_Filtering.py
+++ tests/test_Filtering.py
@@ -11,18 +11,6 @@ def test_string():
     assert isinstance(result_jpg, bytes)
 
 
-def test_log_comp():
-    input1 = io.imread('test.jpg')
-    log_compression('sample', input1)
-    assert os.path.isfile('sample_log.jpg')
-
-
-def test_reverse_video():
-    input1 = io.imread('test.jpg')
-    reverse_video('sample', input1)
-    assert os.path.isfile('sample_rev.jpg')
-
-
 def test_hist():
     input1 = io.imread('test.jpg')
     hist('sample', input1)
@@ -35,7 +23,43 @@ def test_equal_hist():
     assert os.path.isfile('sample1_hist.jpg')
 
 
-def test_contrast_hist():
+def test_contrast_stretch():
     input1 = io.imread('test.jpg')
     contrast_stretching('sample', input1)
     assert os.path.isfile('sample_contrast.jpg')
+
+
+def test_contrast_hist():
+    input1 = io.imread('test.jpg')
+    contrast_hist('sample', input1)
+    assert os.path.isfile('sample2_hist.jpg')
+
+
+def test_raw_hist():
+    input1 = io.imread('test.jpg')
+    raw_hist('sample', input1)
+    assert os.path.isfile('sampleraw_hist.jpg')
+
+
+def test_log_comp():
+    input1 = io.imread('test.jpg')
+    log_compression('sample', input1)
+    assert os.path.isfile('sample_log.jpg')
+
+
+def test_log_hist():
+    input1 = io.imread('test.jpg')
+    log_hist('sample', input1)
+    assert os.path.isfile('sample3_hist.jpg')
+
+
+def test_reverse_video():
+    input1 = io.imread('test.jpg')
+    reverse_video('sample', input1)
+    assert os.path.isfile('sample_rev.jpg')
+
+
+def test_rev_hist():
+    input1 = io.imread('test.jpg')
+    rev_hist('sample', input1)
+    assert os.path.isfile('sample4_hist.jpg')
